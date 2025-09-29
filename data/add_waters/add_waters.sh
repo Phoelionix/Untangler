7b7c8657dc1959dceef11fe0b803c1549acb6665
Phoelionix <spencerkp@hotmail.com>
2025-09-29 20:17:52 +1000

    minor fix for adding new water altlocs

--- data/add_waters/add_waters.sh
+++ data/add_waters/add_waters.sh
@@ -1,7 +1,11 @@
 set -u 
 
+rm -f new.pdb
+
 parent_dir=../../Refinement/tmp_refinement/ 
-handle=best_for_014_loopEnd15
+#handle=best_for_014_loopEnd15
+#handle=best_for_014_WaddedMod_loopEnd1
+handle=best_for_014_WaddedMod_initial
 dir=${parent_dir}${handle}/
 
 cp ${dir}refmacout.pdb .
