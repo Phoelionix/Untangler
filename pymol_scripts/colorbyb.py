05b5aece0d0598b705ea8f403eebfb7f5353c9e4
Phoelionix <spencerkp@hotmail.com>
2025-11-20 16:25:09 +1100

    color only bonds fix for last pymol script

--- pymol_scripts/colorbyb.py
+++ pymol_scripts/colorbyb.py
@@ -19,9 +19,21 @@ EXAMPLE
 
     cmd.show_as('sticks', f"model {model}")
     cmd.set_bond("stick_radius",0.125,model)
-    cmd.set_bond("stick_radius",0.125,f"sidechain and {model}")
-    cmd.color('gray', model)
-    cmd.color('red', f"{model} and b > 0")
+    #cmd.set_bond("stick_radius",0.125,f"sidechain and {model}")
+    bad_bonds = f"badBonds_{model}"
+    model_copy = f"copy_{model}"
+    cmd.create(model_copy, f"{model}")
+    cmd.create(bad_bonds, f"{model_copy} and b > 0")
+    cmd.set_bond("stick_radius",0.2,f"{bad_bonds} and b > 0")
+    cmd.color('gray', model_copy)
+    cmd.color('red', f"{bad_bonds} and b > 0")
+
+    group_name=f"{model}-badbond"
+    cmd.delete(group_name)
+    cmd.group(group_name,f"{model_copy} or {bad_bonds}")
+
+
+
 
 cmd.extend('colorbyb', colorbyb)
 
