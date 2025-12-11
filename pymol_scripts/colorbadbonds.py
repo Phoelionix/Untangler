6620c22ad208f3c407393dd637e0fdd1857bda3e
Phoelionix <spencerkp@hotmail.com>
2025-12-11 19:41:02 +1100

    handling for no bad bonds case for pymol script

--- pymol_scripts/colorbadbonds.py
+++ pymol_scripts/colorbadbonds.py
@@ -17,15 +17,27 @@ EXAMPLE
 
     '''
 
+    def sele_exists(sele):
+        sess = cmd.get_session()
+        for i in sess["names"]:
+            if type(i) == list:
+                if sele==i[0]:
+                    return 1
+        return 0
+
     stick_factor=1.5
 
     #cmd.set_bond("stick_radius",0.125,f"sidechain and {model}")
     bad_bonds = f"badBonds_{model}"
     model_copy = f"copy_{model}"
-    cmd.create(model_copy, f"{model}")
     cmd.create(bad_bonds, f"{model_copy} and b > 0")
+    if not sele_exists(f"model {bad_bonds}"):
+        print("No bad bonds")
+        return 
+        
     for m in [model_copy,bad_bonds]:
         cmd.show_as('sticks', f"model {m}")
+    cmd.create(model_copy, f"{model}")
     #cmd.set_bond("stick_radius",0.2*stick_factor,f"{bad_bonds} and b > 0")
     cmd.set_bond("stick_radius",0.125*stick_factor,model_copy)
     cmd.set_bond("stick_radius",0.16*stick_factor,bad_bonds)
